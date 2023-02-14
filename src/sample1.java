abd9fb15d4b0224cf7860118b2e3f91ada20d54a
Admin <Admin@DESKTOP-12KK9RB>
2023-02-14 14:45:56 +0530

    [Rudresh]:sample1 is ready

---
+++ src/sample1.java
@@ -0,0 +1,15 @@
+
+public class sample1 
+{
+public static void main(String[] args)
+{
+	System.out.println("good morning");
+	
+	System.out.println("have a nice day ");
+	
+	System.out.println("take care");
+	
+	System.out.println("good night");
+	
+}
+}
